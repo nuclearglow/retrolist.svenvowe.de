c953d41abf3018e2ceebf62e337aba7d1e98f39c
Sven Vowe <svenvopwe@gmail.com>
2024-03-29 21:59:02 +0100

    chore: added docs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,9 +5,18 @@ const prisma = new PrismaClient();
 const SEED_LISTS = 10;
 const [SEED_ITEMS_MIN, SEED_ITEMS_MAX] = [5, 20];
 
+/**
+ * Generates a random number between the specified range.
+ * @param start - The start of the range.
+ * @param end - The end of the range.
+ * @returns A random number between the start and end range (inclusive).
+ */
 const randomBetween = (start: number, end: number): number =>
 	Math.floor(Math.random() * (end - start + 1) + start);
 
+/**
+ * Seeds the database with lists and items.
+ */
 async function main() {
 	console.log(`Seeding Database ...`);
 
