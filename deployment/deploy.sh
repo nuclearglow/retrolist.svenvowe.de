35dec60088ed66ecc8abfa5b823acc77e37cdc05
Sven Vowe <svenvowe@gmail.com>
2024-04-20 23:29:47 +0200

    chore: preparing websockets, not running on server yet

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,12 @@
 # add package.json to build folder
 cp -p package.json $LOCAL_PATH
 cp -p package-lock.json $LOCAL_PATH
+# add prisma folder to build folder
 cp -pR prisma $LOCAL_PATH
+# add server setup to build folder TODO: refactor into an own folder so it can be easily copied
+cp -p deployment/prodServer.ts $LOCAL_PATH
+cp -p src/lib/websocket.ts $LOCAL_PATH
+cp -p src/lib/types.ts $LOCAL_PATH
 
 # deploy files
 # TODO: activate after setting everything up
