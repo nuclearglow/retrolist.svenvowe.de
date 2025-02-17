7148f3fc54a313bfc462cfbdf42d5c0a3c503d81
Sven Vowe <svenvowe@gmail.com>
2025-02-17 22:39:09 +0100

    chore: pushed to latest

--- deployment/db-backup.sh
+++ deployment/db-backup.sh
@@ -1,4 +1,5 @@
 # export mysql database from remote
+
 ssh $REMOTE_SERVER "bash -i -c 'cd $REMOTE_DEPLOYMENTS_PATH; mysqldump --defaults-extra-file=$MYSQL_EXTRAS_FILE_PATH -h $MYSQL_HOST -u $MYSQL_USER $MYSQL_DATABASE > $REMOTE_MYSQL_EXPORT_PATH$(date +%F)-$REMOTE_DEPLOYMENT_NAME.prod.sql'"
 
 # sync mysql databases from remote to local
